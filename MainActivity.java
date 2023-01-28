de113b6af990f87a595dbd14765a7e7a5124457d
dmar1313 <122155033+dmar1313@users.noreply.github.com>
2023-01-27 19:29:03 -0600

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,66 @@
+
+// MainActivity.java
+
+public class MainActivity extends 
+AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Login/Signup screen View 
+        loginSignupView = getLayoutInflater().inflate(R.layout.login_signup_view, null);
+        ViewGroup rootView = findViewById(R.id.root_layout);
+        rootView.addView(loginSignupView);
+
+        // Set onClickListener for the signup button
+        Button signupButton = loginSignupView.findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(view -> {
+            // Handle signup
+        });
+
+        // Set onClickListener for the login button
+        Button loginButton = loginSignupView.findViewById(R.id.login_button);
+        loginButton.setOnClickListener(view -> {
+            // Handle login
+        });
+
+        // Bot selection screen
+        View botSelectionView = getLayoutInflater().inflate(R.layout.bot_selection_view, null);
+        ViewGroup rootView = findViewById(R.id.root_layout);
+        rootView.addView(botSelectionView);
+
+        RecyclerView recyclerView = botSelectionView.findViewById(R.id.recycler_view);
+        ArrayList<String> bots = new ArrayList<>();
+        bots.add("Lawyer Bot");
+        bots.add("Idea Bot");
+        bots.add("Work Bot");
+
+        MyAdapter adapter = new MyAdapter(bots);
+        recyclerView.setAdapter(adapter);
+
+        // Set a click listener for each bot in the list
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(context, (view, position) -> {
+            String selectedBot = bots.get(position);
+            // Handle selected bot
+        }));
+
+        // Chat screen
+        View chatView = getLayoutInflater().inflate(R.layout.chat_view, null);
+        ViewGroup rootView = findViewById(R.id.root_layout);
+        rootView.addView(chatView);
+
+        // Set onClickListener for the send button
+        Button sendButton = chatView.findViewById(R.id.send_button);
+        sendButton.setOnClickListener(view -> {
+            // Handle send message
+        });
+
+        // Set onClickListener for the back button
+        Button backButton = chatView.findViewById(R.id.back_button);
+        backButton.setOnClickListener(view -> {
+            // Handle go back to bot selection
+        });
+    }
+}
